7b7c9709589c99de30b6d8a5f9531c78d8da9eeb
qasim16 <qasim@sociabletech.com.au>
2022-05-19 08:58:11 +0500

    UI card

--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -1,22 +1,34 @@
 
-import Header from "../components/Homepage/Header";
+import {useState} from 'react'
+import { MdFilterList } from "react-icons/md";
 import Filter from "../components/Homepage/Filter";
 import Pagination from "../components/common/Pagination";
 import GalleryContainer from "../components/common/GalleryContainer";
 
 function Explore() {
+  const [toggle, setToggle] = useState(false)
   return (
     <>
-  <main className="flex">
-  <section className="w-384">
-    <Filter />
-  </section>
-    <section className="w-full">
-      <GalleryContainer>
-      </GalleryContainer>
-      <Pagination />
-    </section>
-    </main> 
+      <main className="flex">
+        <section className="absolute lg:static w-384">
+          {
+            toggle && <Filter />
+          }
+        </section>
+        <section className="w-full">
+          <section className="container mx-auto px-24 md:px-12 mt-42 mb-26 block lg:hidden">
+            <div className="px--6 flex justify-end">
+            <button onClick={() => setToggle(!toggle)} className="flex items-center gap-2 text-white white-shadow bg-gray-700 transition-all hover:bg-gray-900 rounded-8 py-8 px-26 mt-24">
+              <MdFilterList className="text-22 cursor-pointer text-white" />
+              Filter
+              </button>
+            </div>
+          </section>
+          <GalleryContainer>
+          </GalleryContainer>
+          <Pagination />
+        </section>
+      </main>
     </>
   );
 }
--- src/components/common/GalleryContainer/index.jsx
+++ src/components/common/GalleryContainer/index.jsx
@@ -3,7 +3,7 @@ import GalleryItem from './Item'
 const GalleryContainer = ({children}) => {
 
     return (
-        <section class="container mx-auto px-24 md:px-12 my-58">
+        <section class="container mx-auto px-24 md:px-12 mt-42 mb-58">
             <div className='items-center flex flex-wrap mb-24'>
 
                 <div className='w-full md:w-5/12 ml-auto mr-auto px-4'>
