885ed51bcfb6553f51f7a2164b547b8ce8474346
qasim16 <qasim@sociabletech.com.au>
2022-05-19 12:42:01 +0500

    filter UI

--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -1,12 +1,15 @@
 
-import {useState} from 'react'
-import { MdFilterList } from "react-icons/md";
+import { useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
+import { AiTwotoneFilter } from "react-icons/ai";
 import Filter from "../components/Homepage/Filter";
 import Pagination from "../components/common/Pagination";
 import GalleryContainer from "../components/common/GalleryContainer";
 
 function Explore() {
-  const [toggle, setToggle] = useState(false)
+  const islg = useMediaQuery({ query: '(min-width: 1024px)' })
+
+  const [toggle, setToggle] = useState(islg ? true : false);
   return (
     <>
       <main className="flex">
@@ -16,13 +19,8 @@ function Explore() {
           }
         </section>
         <section className="w-full">
-          <section className="container mx-auto px-24 md:px-12 mt-42 mb-26 block lg:hidden">
-            <div className="px--6 flex justify-end">
-            <button onClick={() => setToggle(!toggle)} className="flex items-center gap-2 text-white white-shadow bg-gray-700 transition-all hover:bg-gray-900 rounded-8 py-8 px-26 mt-24">
-              <MdFilterList className="text-22 cursor-pointer text-white" />
-              Filter
-              </button>
-            </div>
+          <section className="filter-btn lg:hidden flex items-center container mx-auto px-24 md:px-12 h-76">
+            <AiTwotoneFilter onClick={() => setToggle(!toggle)}  className="text-30 cursor-pointer text-white ml-auto" />
           </section>
           <GalleryContainer>
           </GalleryContainer>
