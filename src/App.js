90a4270756f46526d67b5461e9663c2c8145420f
mac <mac@macs-MacBook-Air.local>
2022-06-18 14:28:15 +0500

    changes merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./Pages/Home";
 import Explore from "./Pages/Explore";
 import Artist from "./Pages/Artist";
 import Art from "./Pages/Art";
+import Dashboard from "./Pages/Admin/Dashboard";
 
 function App() {
   return (
     <>
-
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -17,9 +16,8 @@ function App() {
           <Route path="explore" element={<Explore />} />
           <Route path="artist" element={<Artist />} />
         </Route>
+        <Route path="/admin" element={<Dashboard />} />
       </Routes>
-
-
     </>
   );
 }
---
+++ src/Pages/Admin/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+    return(
+        <>
+        <button>meta</button>
+        <button>wallet connection</button>
+        </>
+    )
+}
+export default Dashboard
