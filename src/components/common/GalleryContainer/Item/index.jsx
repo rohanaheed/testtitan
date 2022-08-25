52c8e4ded87b7dacf40bc51c1ce331cb4f22c3bd
Salman Saleem <msalmansaleem1998@gmail.com>
2022-08-25 13:14:00 +0500

    Gallery done.

--- src/components/common/GalleryContainer/Item/index.jsx
+++ src/components/common/GalleryContainer/Item/index.jsx
@@ -7,9 +7,11 @@ const GalleryItem = ({ item }) => {
         <div className="cursor-pointer my-18 w-full md:w-1/2 lg:my-12 px-8 lg:w-1/3">
             <Link to="/art">
                 <article className="card overflow-hidden rounded-lg shadow-lg">
-                    <a href="#">
-                        <img alt="Placeholder" className="block h-auto w-full" src={item?.image ? item?.image : "https://picsum.photos/600/400/?random"} />
-                    </a>
+                    <div className="img-container">
+                        <a href="#">
+                            <img alt="Placeholder" className="block h-auto w-full" src={item?.image ? item?.image : "https://picsum.photos/600/400/?random"} />
+                        </a>
+                    </div>
                     <footer className='py-18 px-6'>
                         <h6 className="mt-4 text-lg leading-relaxed text-gray-700 cursor-pointer font-medium transition-all hover:text-gray-900"> {item?.name} </h6>
                         <p className="text-lg leading-relaxed text-gray-700"> {item?.artistName} </p>
