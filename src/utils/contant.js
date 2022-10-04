c0dd8916bbc282174fd4be4bbde91849beafb0c3
mac <mac@macs-MacBook-Air.local>
2022-10-04 23:36:28 +0500

    changes merge

--- src/utils/contant.js
+++ src/utils/contant.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://3.234.96.47:9091/'
-export const API_URL_ADMIN = 'http://3.234.96.47:9091/'
+export const API_URL = 'https://api.qaia.io/'
+export const API_URL_ADMIN = 'https://api.qaia.io/'
