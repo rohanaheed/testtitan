98af7e287d38c3befbfaeb989ad21a360c9ca76c
mac <mac@macs-MacBook-Air.local>
2022-10-05 23:42:26 +0500

    changes merge

--- src/components/common/ConnectionModel.jsx
+++ src/components/common/ConnectionModel.jsx
@@ -33,7 +33,7 @@ const ConnectionModel = ({ popup, onClose }) => {
   const isxs = useMediaQuery({ query: '(min-width: 486px)' })
 
   const customStyles = {
-    content: { position: 'relative', inset: '0', backgroundColor: '#FFFFFF', border: 'none', borderRadius: '5px', overflow: 'none', padding: '0' }
+    content: { position: 'relative', color: 'white', inset: '0', backgroundColor: 'black', border: 'none', borderRadius: '5px', overflow: 'none', padding: '0' }
   }
   return (
     <ReactModal
@@ -41,24 +41,24 @@ const ConnectionModel = ({ popup, onClose }) => {
       style={customStyles}
       onRequestClose={() => handleClose()}
     >
-      <h2 className="text-center text-24 text-gray-800 mt-58"> Connect Wallet </h2>
-      <p className="text-gray-600 mb-34 text-center px-24 lg:px-72"> Connect with one of our wallet providers or create new one</p>
-      <div class="text-sm text-gray-500 px-28 border-gray-200 mb-79">
-        <ul class="rounded-8 card-shadow bg-gray-50 w-full mx-auto my-60 border">
+      <h2 className="text-center text-24 text-white mt-58"> Connect Wallet </h2>
+      <p className="text-white mb-34 text-center px-24 lg:px-72"> Connect with one of our wallet providers or create new one</p>
+      <div class="text-sm text-white px-28 border-gray-200 mb-79">
+        <ul class="rounded-8 card-shadow bg-black w-full mx-auto my-60 border">
           <li onClick={() => connect('meta')} class="wallet-item flex items-center justify-between px-26 py-20 border-b cursor-pointer">
-            <div class=" flex justify-between text-gray-700 items-center gap-5 font-normal text-16 cursor-pointer transition-all">
+            <div class=" flex justify-between text-white bg-black items-center gap-5 font-normal text-16 cursor-pointer transition-all">
               <img class="w-24" src="/assets/metamask.webp" alt="" />
               MetaMask</div>
-            <div class="bg-primary text-gray-800 px-16 py-3 rounded-20">
+            <div class="bg-white text-gray-800 px-16 py-3 rounded-20">
               Popular
             </div>
           </li>
           <li onClick={() => connect('')} class="wallet-item flex items-center justify-between px-26 py-20 border-b cursor-pointer">
-            <div class=" flex justify-between text-gray-700 items-center gap-5 font-normal text-16 cursor-pointer transition-all">
+            <div class=" flex justify-between text-white bg-black items-center gap-5 font-normal text-16 cursor-pointer transition-all">
               <img class="w-24" src="https://ik.imagekit.io/xanalia/Images/WalletConnect.svg" alt="" />
               MetaMask
             </div>
-            <div class="bg-primary text-gray-800 px-16 py-3 rounded-20">
+            <div class="bg-white text-gray-800 px-16 py-3 rounded-20">
               Popular
             </div>
           </li>
@@ -66,8 +66,8 @@ const ConnectionModel = ({ popup, onClose }) => {
           {/* <li class="wallet-item flex items-center justify-between px-26 py-22 border-b cursor-pointer"><div class=" flex justify-between text-gray-700 items-center gap-5 font-normal text-16 cursor-pointer transition-all"><img class="w-24" src="/assets/phantom.webp" alt="" />Phantom</div></li> */}
         </ul>
       </div>
-      <div onClick={() => connect('meta')} className="bg-gray-900 py-18 px-28 mt-44 cursor-pointer">
-        <p className="text-gray-200 font-medium text-center"> Connect Wallet </p>
+      <div onClick={() => connect('meta')} className="bg-white text-black py-18 px-28 mt-44 cursor-pointer">
+        <p className="text-black-300 font-medium text-center"> Connect Wallet </p>
       </div>
       {/* <div className="wallet-modal">
         <div style={{ display: 'none' }}>
