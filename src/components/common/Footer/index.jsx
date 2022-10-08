b2088be45860bc3da4925727aeb646eeeb0b5fc7
mac <mac@macs-MacBook-Air.local>
2022-10-08 16:05:29 +0500

    changes merge

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <span className="text-sm text-gray-300 sm:text-center ml-8">© 2022 TITAN Marketplace™ All Rights Reserved.</span>
                 <div className="flex mt-4 space-x-6 justify-center md:mt-0">
                     <span onClick={() => history.push('/privacy')} className="cursor-pointer text-gray-400 text-14 hover:underline transition-all">Privacy Policy</span>
-                    <span className="cursor-pointer text-gray-400 text-14 hover:underline transition-all">Terms of Service</span>
+                    <span onClick={() => history.push('/term')} className="cursor-pointer text-gray-400 text-14 hover:underline transition-all">Terms of Service</span>
                 </div>
             </section>
         </footer>
